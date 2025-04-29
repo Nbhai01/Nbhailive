b2d0d39be6264b715de991efae6eb4fd877f2b37
Nbhai01 <mr.heartless0143@gmail.com>
2025-04-29 11:09:17 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const cacheName = 'mic-earphones-cache-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(assetsToCache))
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
